22c161c7a6d45a9c614ac362e413ae8ea7dacc78
Matt Phillips <mathewrphillips@gmail.com>
2016-09-19 19:44:40 -0500

    temp for brad

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace Receiptze
 {
@@ -6,5 +9,35 @@ namespace Receiptze
     {
         [HttpGet("/")]
         public IActionResult Index() => View();
+
+        [HttpGet("/Test")]
+        public IActionResult Test()
+        {
+            try{
+                var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+                using(var connection = factory.CreateConnection())
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "hello",
+                                        basicProperties: null,
+                                        body: body);
+                
+                }
+                return View("Healthy");
+            }catch(Exception e){
+                ViewBag.Error = e.Message;
+                return View("NotHealthy");
+            }
+        }
     }
 }
