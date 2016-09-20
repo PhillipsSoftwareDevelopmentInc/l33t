53cd6ff3fcb92c5b4ef1d06e93bbadc5de7949d2
Matt Phillips <mathewrphillips@gmail.com>
2016-09-20 11:10:32 -0500

    Got StartBatchCommand messages to be consumed in initializer

--- initializer/Program.cs
+++ initializer/Program.cs
@@ -18,29 +18,32 @@ namespace Initializer
             Console.WriteLine("Starting Initializer");
             var factory = new ConnectionFactory() { HostName = "rabbit", Port = 5672 };
             using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "StartBatchCommand",
-                                    durable: false,
-                                    exclusive: false,
-                                    autoDelete: false,
-                                    arguments: null);
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
+                using(var channel = connection.CreateModel())
                 {
-                    Console.WriteLine("Message Received");
-                    var body = ea.Body;
-                    var msg = Encoding.UTF8.GetString(body);
-                    var message = JsonConvert.DeserializeObject<StartBatchCommand>(msg);
-                    Console.WriteLine($"Start Batch Processed {message.BatchId} : {message.StartDate}");
-                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-                };
-                channel.BasicConsume(queue: "StartBatchCommand",
-                                 noAck: false,
-                                 consumer: consumer);
+                    channel.QueueDeclare(queue: "StartBatchCommand",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        Console.WriteLine("Message Received");
+                        var body = ea.Body;
+                        var msg = Encoding.UTF8.GetString(body);
+                        var message = JsonConvert.DeserializeObject<StartBatchCommand>(msg);
+                        Console.WriteLine($"Start Batch Processed {message.BatchId} : {message.StartDate}");
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+                    channel.BasicConsume(queue: "StartBatchCommand",
+                                    noAck: false,
+                                    consumer: consumer);
 
+                    System.Threading.Thread.Sleep(100000);
+                }
             }
-            Console.ReadLine();
+            
         }
     }
 }
